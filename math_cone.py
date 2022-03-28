77d9b630680deb2ff84aa5aec6267afa65f40a87
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-28 20:22:33 +0000

    surface and volume cone

---
+++ math_cone.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Created by Yiyun Qin
+# Created on March 2022
+# This is the math program, with proper style
+
+import math
+from fractions import Fraction
+
+
+def main():
+    # This function calculates the surface area and volume of a cone
+
+    # input
+    radius = int(input("Enter the radius of the cone you want to calculate: "))
+    height = int(input("Enter the height of the cone you want to calculate: "))
+
+    # process
+    surface_area = (
+        math.pi
+        * radius
+        * (radius + pow((pow(height, 2) + pow(radius, 2)), Fraction(1, 2)))
+    )
+    volume = math.pi * pow(radius, 2) * height * Fraction(1, 3)
+
+    # output
+    print("")
+    print("The surface area of the cone is {:,.2f} cm².".format(surface_area))
+    print("The volume of the cone is {:,.2f} cm\u00b3.".format(volume))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
